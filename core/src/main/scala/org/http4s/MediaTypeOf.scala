59e94cb2315a7ac04800f6ad5ea5a9d69efabb77
rossabaker <ross@rossabaker.com>
2015-01-08 11:57:11 -0800

    A MediaTypeOf approach to inferring Elem's media type.

---
+++ core/src/main/scala/org/http4s/MediaTypeOf.scala
@@ -0,0 +1,3 @@
+package org.http4s
+
+final case class MediaTypeOf[T](mediaType: MediaType)
--- scala-xml/src/main/scala/org/http4s/scalaxml/package.scala
+++ scala-xml/src/main/scala/org/http4s/scalaxml/package.scala
@@ -1,3 +1,11 @@
 package org.http4s
 
-package object scalaxml extends ElemInstances
+import scala.xml.Elem
+
+package object scalaxml extends ElemInstances {
+  object implicits {
+    implicit val ElemIsXml = MediaTypeOf[Elem](MediaType.`application/xml`)
+    implicit val ElemIsHtml = MediaTypeOf[Elem](MediaType.`text/html`)
+    implicit val ElemIsXhtml = MediaTypeOf[Elem](MediaType.`application/xhtml+xml`)
+  }
+}
