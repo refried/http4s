9ce225bd69c0fe3912f9b38605fdb11d599b00cf
Arya Irani <arya.irani@gmail.com>
2016-01-08 14:17:22 -0500

    property checks for UrlFormCodec

--- core/src/test/scala/org/http4s/util/UrlFormCodecSpec.scala
+++ core/src/test/scala/org/http4s/util/UrlFormCodecSpec.scala
@@ -1,11 +1,33 @@
 package org.http4s.util
 
+import org.scalacheck.Prop
 import org.specs2.{ScalaCheck, Specification}
+import UrlCodingUtils.urlEncode
+
+import scalaz.NonEmptyList
+import scalaz.scalacheck.ScalazArbitrary._
 
 class UrlFormCodecSpec extends Specification with ScalaCheck {
-  def is = s2"""
+  def is = {
+    s2"""
     UrlFormCodec.encode should include & in multi-valued attributes ${
-      UrlFormCodec.encode(Map("foo" -> Seq("a", "b"))) should_== "foo=a&foo=b"
+      Prop.forAll { (keyChars: NonEmptyList[Char], values: NonEmptyList[String]) =>
+        val keyString: String = keyChars.list.mkString
+        UrlFormCodec.encode(Map(keyString -> values.list)) ===
+          values.map(urlEncode(_)).list
+            .mkString(
+              urlEncode(keyString) + '=',
+              '&' + urlEncode(keyString) + '=',
+              ""
+            )
+      }
+    }
+    UrlFormCodec.encode shouldn't include & in no-valued attributes ${
+      Prop.forAll(
+        (key: String) =>
+          UrlFormCodec.encode(Map(key -> Nil)) === urlEncode(key)
+      )
     }
   """
+  }
 }
